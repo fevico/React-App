15ef6efbb03ce46a105d68f71f5bbda30738d020
Ajayi favour <ajayifavour81@gmail.com>
2023-11-30 17:04:21 +0100

    Event and useState

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,7 @@
-function Item ({item}) {
+function Item ({item, selectProduct}) {
     return(
         <>
-             <div className={`product ${item.isInBag ? 'selected' : '' }`}>
+             <div onClick={()=> selectProduct(item.id)} className={`product ${item.isInBag ? 'selected' : '' }`}>
                     <div className="photo">
                         <img src={"./img/" + item.photo} />
                     </div>
